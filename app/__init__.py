858971bd5cb10ddcf90769c5d1c1bda26c0847db
Tram <tramnhoang@gmail.com>
2024-11-05 22:43:36 -0800

    hello book refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,26 @@
 from flask import Flask
 from .db import db, migrate
 from .models import book
-from .routes.book_routes import books_bp
+from .routes.book_routes import bp as books_bp
+from .routes.author_routes import bp as authors_bp
+import os
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    if config:
+        # Merge 'config' into the app's configuration
+        # to override the app's default settings
+        app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
     app.register_blueprint(books_bp)
+    app.register_blueprint(authors_bp)
 
     return app
-
---
+++ tests/test_route_utilities.py
@@ -0,0 +1,92 @@
+from werkzeug.exceptions import HTTPException
+from app.routes.route_utilities import validate_model, create_model, get_models_with_filters
+from app.models.book import Book
+from app.models.author import Author
+import pytest
+
+def test_validate_model(two_saved_books):
+    # Act
+    result_book = validate_model(Book, 1)
+
+    # Assert 
+    assert result_book.id == 1
+    assert result_book.title == "Ocean Book"
+    assert result_book.description == "watr 4evr"
+
+def test_validate_model_missing_record(two_saved_books):
+    # Act & Assert
+    # Calling `validate_model` without being invoked by a route will
+    # cause an `HTTPException` when an `abort` statement is reached 
+    with pytest.raises(HTTPException) as error:
+        result_book = validate_model(Book, "3")
+    
+    response = error.value.response
+    assert response.status == "404 NOT FOUND"
+
+def test_validate_model_invalid_id(two_saved_books):
+    # Act & assert
+    with pytest.raises(HTTPException) as error:
+        result_book = validate_model(Book, "two")
+    
+    response = error.value.response
+    assert response.status == "400 BAD REQUEST"
+
+def test_create_model_book(client):
+    # Arrange
+    test_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+    # Act
+    result = create_model(Book, test_data)
+
+    # Assert
+    assert result.status_code == 201
+    assert result.get_json() == {
+        "id": 1,
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+def test_create_model_book_missing_data(client):
+    # Arrange
+    test_data = {
+        "description": "The Best!"
+    }
+
+    # Act & Assert
+    # Calling `create_model` without being invoked by a route will
+    # cause an `HTTPException` when an `abort` statement is reached 
+    with pytest.raises(HTTPException) as error:
+        result_book = create_model(Book, test_data)
+
+    response = error.value.response
+    assert response.status == "400 BAD REQUEST"
+
+def test_create_model_author(client):
+    # Arrange
+    test_data = {
+        "name": "New Author"
+    }
+
+    # Act
+    result = create_model(Author, test_data)
+
+    # Assert
+    assert result.status_code == 201
+    assert result.get_json() == {
+        "id": 1,
+        "name": "New Author"
+    }
+
+def test_get_models_with_filters_one_matching_book(two_saved_books):
+    # Act
+    result = get_models_with_filters(Book, {"title": "ocean"})
+
+    # Assert
+    assert result == [{
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }]
